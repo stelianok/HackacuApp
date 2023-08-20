042d0bb59bac4075894e8fc4218cb6769428a795
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-08-20 12:07:59 -0300

    feat: add header component

--- App.tsx
+++ App.tsx
@@ -3,14 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import { Header } from "./src/components/Header";
 
 export default function App() {
-  return <Header />;
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    height: 300,
   },
 });
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,10 +1,33 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export const styles = StyleSheet.create({
-    view: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  header: {
+    backgroundColor: "#C66917",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: "#FFF",
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+  top: {
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: 30,
+    color: "white",
+  },
+  subtitle: { 
+    color: "white", 
+    fontSize: 13,
+    paddingTop: 4,
+},
+  image: {
+    width: 48,
+    height: 48,
+    borderRadius: 10,
+  },
+});
