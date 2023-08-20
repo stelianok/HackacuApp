c26061246f5de069fc7906c89b9272fa3b32d1b7
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-08-20 10:37:35 -0300

    first commit in new branch

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./style";
+
+export const Header = () => {
+  return (
+    <View style={styles.view}>
+      <Text>hello header</Text>
+    </View>
+  );
+};
---
+++ src/components/ListBolets/style.ts
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+export const styles = StyleSheet.create({
+    view: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
